333f0e9ef98b03e60fe62f1f1d0347ef88f4b14d
toyobayashi <lifenglin314@outlook.com>
2021-10-21 22:53:48 +0800

    1

---
+++ build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ "x$1" == "x" ]; then
+  echo "Missing node version"
+  exit 1
+fi
+
+ndkver=
+
+if [ "x$2" == "x" ]; then
+  ndkver="23.0.7599858"
+else
+  ndkver="$2"
+fi
+
+nodever="$1"
+tarfile="v$nodever.zip"
+
+if [ ! -f "$tarfile" ]; then
+  curl -OL "https://github.com/nodejs/node/archive/refs/tags/$tarfile"
+  # curl -OL "https://hub.fastgit.org/nodejs/node/archive/refs/tags/$tarfile"
+fi
+
+unzip -d . "$tarfile"
+patch -p0 < ./android-configure.patch
+
+dir="node-$nodever"
+cd "$dir"
+chmod +x ./android-configure
+./android-configure "$ANDROID_HOME/ndk/$ndkver" arm64 23
+make
+
+outdir="build"
+mkdir -p "../$outdir/lib"
+HEADERS_ONLY=1 python3 ./tools/install.py install "../$outdir" /
+cp -rpf "./out/Release/libnode.so" "../$outdir/lib/libnode.so"
+cd ..
+
+zipname="build-$nodever.zip"
+
+rm -rf "$zipname"
+
+type zip >/dev/null 2>&1
+if [ "x$?" == "x0" ]; then
+  cd "$outdir"
+  zip -r -y "../$zipname" .
+  cd ..
+else
+  powershell.exe -nologo -noprofile -command \
+    '& { param([String]$sourceDirectoryName, [String]$destinationArchiveFileName, [Boolean]$includeBaseDirectory); Add-Type -A "System.IO.Compression.FileSystem"; Add-Type -A "System.Text.Encoding"; [IO.Compression.ZipFile]::CreateFromDirectory($sourceDirectoryName, $destinationArchiveFileName, [IO.Compression.CompressionLevel]::Fastest, $includeBaseDirectory, [System.Text.Encoding]::UTF8); exit !$?;}' \
+    -sourceDirectoryName "\"$outdir\"" \
+    -destinationArchiveFileName "$zipname" \
+    -includeBaseDirectory '$false'
+  if [ $? -ne 0 ]; then
+    echo "Zip failed"
+    exit $?
+  fi
+fi
+
+rm -rf "$outdir"
+rm -rf "$dir"
+# rm -rf "$tarfile"
